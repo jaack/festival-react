9c28b9329dc47d8ac4fed68a2bf2faa9b3d88b05
Jack Burns <chugga151@gmail.com>
2020-02-26 20:58:25 +0000

    initial commit for testing

---
+++ precache-manifest.caad89bd13f6342d8dd6a5ce3cd32719.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdde39da4741532821180673466b88ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e78c5340ecba40f578f6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4dc23de04b0ff09c0da9",
+    "url": "/static/js/2.c25c3375.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.c25c3375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78c5340ecba40f578f6",
+    "url": "/static/js/main.7c2aff20.chunk.js"
+  },
+  {
+    "revision": "a8547ee2347c118cbafb",
+    "url": "/static/js/runtime-main.aeebf5fc.js"
+  }
+]);
